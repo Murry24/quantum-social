8394d39029c09f7cde0e3c9721adc7328b1278c2
Maroš Maury <tvoje-noreply@users.noreply.github.com>
2025-09-13 17:45:10 +0200

    fix: add MDC for Theme.Material3.DayNight.NoActionBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended") // kvôli ikonke hviezdy
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("com.google.android.material:material:1.12.0")
+
 }
 
 ktlint {
